fa8346c90f3efd372165f34a3358eb072fa12cba
JOPA167 <joparajirj@gmail.com>
2026-01-08 12:42:15 -0500

    Corte del taller

--- ejercicios/Problemas/src/paquete04/Problema4.java
+++ ejercicios/Problemas/src/paquete04/Problema4.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package paquete04;
-
+import java.util.Scanner;
 /**
  *
  * @author reroes
@@ -15,7 +15,41 @@ public class Problema4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        String nombre;
+        String cedula;
+        System.out.println("Ingrese la opcion que desea utilizar:1. Calcular Valor"
+                + "Luz 2.Calcular Predio");
+        opcion = entrada.nextInt();
+        if (opcion>0 & opcion <=3){
+            System.out.println("Ingrese su nombre: ");
+            nombre = entrada.nextLine();
+            System.out.println("Ingrese su cedula: ");
+            cedula = entrada.nextLine();
+            switch (opcion){
+                case 1:
+                    obtenerAreaCuadrado();
+                    break;
+                case 2:
+                    obtenerAreaTriangulo();
+                    break;
+                
+            }
+        }else{
+            System.out.println("LA OPCIÓN INGRESADA NO ES COMPATIBLE");
+        }
     }
+    public static String calcularValorLuz(String n,String C){
+        Scanner entrada = new Scanner(System.in);
+        double valorK;
+        int numeroK;
+        System.out.println("Ingrese el valor del kilowatio: ");
+        valorK = entrada.nextDouble();
+        System.out.println("Ingrese el número de kilowatio: ");
+        numeroK = entrada.nextInt();
+        
+    }
+    
     
 }
